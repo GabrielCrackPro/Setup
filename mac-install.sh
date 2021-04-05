a0fcc33ac25911218218862cadcb529f8e22d677
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-04-05 11:20:05 +0200

    Install script updated ✏

--- mac-install.sh
+++ mac-install.sh
@@ -1,14 +1,14 @@
-xcode-select --install
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew update
-brew cask install iterm2
-brew install fortune
-brew install cowsay
-brew install git
-brew install vcprompt
-brew cask install spectacle
-brew cask install alfred
-brew cask install firefox
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-nvm install stable
-brew cask install visual-studio-code
+softwareupdate -l # check for system updates
+xcode-select --install # install xcode tools
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" # install homebrew
+brew update #update homebrew packages
+brew cask install iterm2 # install iTerm2 terminal emulator
+brew install fortune # install fotune for inspirational quotes
+brew install cowsay # install cowsay. A talking cow
+brew install git # install git version control
+brew cask install spectacle # install spectacle window manager
+brew cask install alfred # install alfred to replace spotlight
+brew cask install firefox # install firefox
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh # install node version manager
+nvm install stable # install last stable version of nodejs
+brew cask install visual-studio-code # install vscode
