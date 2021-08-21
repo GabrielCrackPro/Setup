95df7a1c25e1ee5f948ce53951a2d6f7cac1a95a
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-08-21 20:16:14 +0200

    ReadMe Updated ✏️

--- mac-install.sh
+++ mac-install.sh
@@ -6,7 +6,7 @@ brew cask install iterm2 # install iTerm2 terminal emulator
 brew install fortune # install fotune for inspirational quotes
 brew install cowsay # install cowsay. A talking cow
 brew install git # install git version control
-brew cask install spectacle # install spectacle window manager
+brew cask install rectangle # install rectangle window manager
 brew cask install alfred # install alfred to replace spotlight
 brew cask install firefox # install firefox
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh # install node version manager
